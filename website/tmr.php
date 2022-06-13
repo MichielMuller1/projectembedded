007ae713737e93535d73ece20638179c50abd8c4
Ilias Safiullin <r0832668@student.thomasmore.be>
2022-06-13 14:00:43 +0200

    ja

---
+++ website/tmr.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$servername = "localhost";
+
+// REPLACE with your Database name
+$dbname = "laptops";
+// REPLACE with Database user
+$username = "ubuntu";
+// REPLACE with Database user password
+$password = "Patje123";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
