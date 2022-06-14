9787d9ae3ff01b9277b417a3bbf0b55d92ec302c
Ilias Safiullin <r0832668@student.thomasmore.be>
2022-06-14 15:02:03 +0200

    ok

--- website/slechtehuurders.php
+++ website/slechtehuurders.php
@@ -16,7 +16,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM kaarten, kastjes, openKastjes, slechteHuurders";
+$sql = "SELECT * FROM slechteHuurders";
 
 if ($result = $conn->query($sql)) {
     while ($row = $result->fetch_assoc()) {
