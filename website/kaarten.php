3ed7d2c92cfa05aae2759dbc133ae075d3028832
Michiel Muller <71765474+MichielMuller1@users.noreply.github.com>
2022-06-15 00:59:18 +0200

    Update kaarten.php

--- website/kaarten.php
+++ website/kaarten.php
@@ -19,25 +19,26 @@ if ($conn->connect_error) {
 $sql = "SELECT * FROM kaarten";
 
 if ($result = $conn->query($sql)) {
+    $arr = [];
     while ($row = $result->fetch_assoc()) {
         $row_uid = $row["UID"];
         $row_rnummer = $row["rnummer"];
 		$row_voornaam = $row["voornaam"];
         $row_achternaam = $row["achternaam"];
 	
+
 		
-		
-		
-		
-		$data = array(
+	$data = array(
 		'uid' => $row_uid,
 		'rnummer' => $row_rnummer, 
 		'voornaam' => $row_voornaam, 
 		'achternaam' => $row_achternaam
 			     );
-		header('Content-type: application/json');
-		echo json_encode($data);
+    $arr += $data;
     }
+	header('Content-type: application/json');
+	echo json_encode($arr);
+    
     $result->free();
 }
 
