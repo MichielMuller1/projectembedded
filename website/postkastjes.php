d32f1c99ecfb1392ed31b034d092e29b1a3463cb
Ilias Safiullin <r0832668@student.thomasmore.be>
2022-06-14 22:52:30 +0200

    50 estay usa

---
+++ website/postkastjes.php
@@ -0,0 +1,66 @@
+<?php
+
+$servername = "localhost";
+
+// REPLACE with your Database name
+$dbname = "laptops";
+// REPLACE with Database user
+$username = "ubuntu";
+// REPLACE with Database user password
+$password = "Patje123";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+// Keep this API Key value to be compatible with the ESP32 code provided in the project page. 
+// If you change this value, the ESP32 sketch needs to match
+$api_key_value = "tPmAT5Ab3j7F7";
+
+$api_key= $oplaadstatus = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $api_key = test_input($_POST["api_key"]);
+    if($api_key == $api_key_value) {
+        $oplaadstatus = test_input($_POST["oplaadStatus"]);
+
+
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+        $t=time();
+        
+        $sql = "UPDATE kastjes SET tijd = '".date("Y-m-d H:i:s",$t)."', oplaadStatus = '" .$oplaadstatus. "' WHERE id = '1'";
+        
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } 
+        else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    
+        $conn->close();
+    }
+    else {
+        echo "Wrong API Key provided.";
+    }
+
+}
+else {
+    echo "No data posted with HTTP POST.";
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
