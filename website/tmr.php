f37251e3a3096dff9bb6ec024247a7743ac06eaa
Ilias Safiullin <r0832668@student.thomasmore.be>
2022-06-14 14:25:49 +0200

    and this message #2 por da project

--- website/tmr.php
+++ website/tmr.php
@@ -16,7 +16,53 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM kastjes";
+$sql = "SELECT * FROM kaarten, kastjes, openKastjes, slechteHuurders";
+
+if ($result = $conn->query($sql)) {
+    while ($row = $result->fetch_assoc()) {
+        $row_uid = $row["uid"];
+        $row_rnummer = $row["rnummer"];
+		$row_voornaam = $row["voornaam"];
+        $row_achternaam = $row["achternaam"];
+	$row_kastNr = $row["kastNr"];
+    $row_oplaadStatus = $row["oplaadStatus"];
+    $row_solenoidStatus = $row["solenoidStatus"];
+    $row_magneetStatus = $row["magneetStatus"]; 
+	$row_uitgeleend = $row["uitgeleend"]; 
+	$row_uitgeleendDoor = $row["uitgeleendDoor"]; 
+	$row_uitgeleendDatum = $row["uitgeleeendDatum"];
+	$row_kastNrOpen = $row["kastNrOpen"];
+	$row_rnummerHuurder = $row["rnummerHuurder"];
+	$row_aantalFouten = $row["aantalFouten"];
+	$row_rnummerTerugbrenger = $row["rnummerTerugbrenger"];
+	
+		
+		
+		
+		
+		$data = array(
+		'uid' => $row_uid,
+		'rnummer' => $row_rnummer, 
+		'voornaam' => $row_voornaam, 
+		'achternaam' => $row_achternaam, 
+		'kastNr' => $row_kastNr, 
+		'oplaadStatus' => $row_oplaadStatus,
+		'solenoidStatus' => $row_solenoidStatus,
+		'magneetStatus' => $row_magneetStatus,
+		'uitgeleend' => $row_uitgeleend,
+		'uitgeleendDoor' => $row_uitgeleendDoor,
+		'uitgeleendDatum' => $row_uitgeleendDatum,
+		'kastNrOpen' => $row_kastNrOpen,
+		'rnummmerHuurder' => $row_rnummerHuurder,
+		'aantalFouten' => $row_aantalFouten,
+		'rnummerTerugbrenger' => $row_rnummerTerugbrenger
+			     );
+		header('Content-type: text/javascript');
+		echo json_encode($data);
+    }
+    $result->free();
+}
 
 
+$conn->close();
 ?>
