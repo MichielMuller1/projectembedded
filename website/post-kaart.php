d6f451483722a1935033cf48bc9562ec334d6db0
Michiel Muller <71765474+MichielMuller1@users.noreply.github.com>
2022-06-14 23:31:35 +0200

    Update post-kaart.php

--- website/post-kaart.php
+++ website/post-kaart.php
@@ -10,7 +10,7 @@ $api_key= $num = $rnum = $vn = $an = $ad = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $api_key = test_input($_POST["api_key"]);
-    if($api_key == $api_key_value) {
+    // if($api_key == $api_key_value) {
         $num = test_input($_POST["num"]);
         $rnum = test_input($_POST["rnum"]);
         $vn = test_input($_POST["vn"]);
@@ -35,10 +35,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     
         $conn->close();
-    }
-    else {
-        echo "Wrong API Key provided.";
-    }
+    // }
+    // else {
+    //     echo "Wrong API Key provided.";
+    // }
 
 }
 else {
