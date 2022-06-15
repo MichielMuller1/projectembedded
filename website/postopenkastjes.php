7008fbb7f587a38584dfafc61a4db2bd3f9386f4
Michiel Muller <71765474+MichielMuller1@users.noreply.github.com>
2022-06-15 17:28:54 +0200

    Update postopenkastjes.php

--- website/postopenkastjes.php
+++ website/postopenkastjes.php
@@ -38,7 +38,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         } 
         $t=time();
         
-        $sql = "UPDATE kastjes SET tijd = '".date("Y-m-d H:i:s",$t)."', kastNr = '".$kastnr."' WHERE id = '1'";
+        $sql = "UPDATE openKastjes SET tijd = '".date("Y-m-d H:i:s",$t)."', kastNr = '".$kastnr."' WHERE id = '1'";
         
         if ($conn->query($sql) === TRUE) {
             echo "New record created successfully";
